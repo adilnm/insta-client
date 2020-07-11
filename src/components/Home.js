544cd5638eb9767d406bcfc1f985271204658e56
Adil Naimi <adilnm23@gmail.com>
2020-07-11 00:04:46 -0500

    Styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React from "react";
 
-const Home=()=>{
-    return(
-        <h1>Home</h1>
-    )
-}
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="card home-card">
+        <h5>Jon</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
 
-export default Home
+          <h6>Title</h6>
+          <p>This is an amzing post</p>
+          <input type="text" placeholder="Add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Jon</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>Title</h6>
+          <p>This is an amzing post</p>
+          <input type="text" placeholder="Add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Jon</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+
+          <h6>Title</h6>
+          <p>This is an amzing post</p>
+          <input type="text" placeholder="Add a comment" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React from "react";
 
-const Profile=()=>{
-    return(
-        <h1>Profile</h1>
-    )
-}
+const Profile = () => {
+  return (
+    <div style={{maxWidth:"550px", margin:"0px auto"}}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "18px,0px",
+          borderBottom: "1px solid grey"
+        }}
+      >
+        <div>
+          <img
+            style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+            src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+            alt="profile pic"
+          />
+        </div>
+        <div>
+          <h4>Jon Jemes</h4>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "108%"
+            }}
+          >
+            <h6>40 posts</h6>
+            <h6>40 followers</h6>
+            <h6>40 following</h6>
+          </div>
+        </div>
+      </div>
+      <div className="gallery">
+        <img className="item"
+          src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          alt=""
+        />
+        <img className="item"
+          src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          alt=""
+        />
+        <img className="item"
+          src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          alt=""
+        />
+        <img className="item"
+          src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          alt=""
+        />
+        <img className="item"
+          src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          alt=""
+        />
+        <img className="item"
+          src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          alt=""
+        />
 
-export default Profile
+      </div>
+    </div>
+  );
+};
+export default Profile;
